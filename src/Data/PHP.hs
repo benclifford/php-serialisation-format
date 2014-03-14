1aa89a7d2c15bbe4c078aa2c08930a51ca8e97a4
Ben Clifford <benc@hawaga.org.uk>
2014-03-14 11:06:49 +0000

    initial import

---
+++ src/Data/PHP.hs
@@ -0,0 +1,64 @@
+
+-- | http://www.phpinternalsbook.com/classes_objects/serialization.html
+
+module Data.PHP where
+
+  import Control.Applicative
+  import Control.Monad
+  import Text.ParserCombinators.Parsec hiding ( (<|>) , many )
+
+  data PHPData =
+      PHPArray [(PHPData, PHPData)]
+    | PHPString String
+    deriving Show
+
+  phpdata =
+        (phparray <?> "PHP array")
+    <|> (phpstring <?> "PHP string")
+
+  phpstring = do
+    char 's'
+    char ':'
+    strlen <- integer
+    char ':'
+    char '"'
+    v <- forM [1..strlen] $ \_ -> anyChar
+    char '"'
+    char ';'
+    return $ PHPString v
+
+  phparray = do
+    char 'a'
+    char ':'
+    count <- integer
+    char ':'
+    char '{'
+    v <- forM [1..count] $ \_ -> do
+      key <- phpdata
+      value <- phpdata
+      return (key, value)
+    char '}'
+
+    return $ PHPArray v
+
+  integer = do
+    digits <- many $ oneOf "0123456789"
+    return (read digits :: Int)
+
+  parsePHP :: String -> Either ParseError PHPData
+  parsePHP s = parse (phpdata <* eof) "(unknown)" s
+
+  testStr1 = "a:0:{}"
+  testStr2 = "a:2:{s:7:\"booking\";a:1:{s:15:\"booking_comment\";s:0:\"\";}s:7:\"gateway\";s:6:\"realex\";}"
+  testStr3 = "s:5:\"hello\";"
+
+  main = do
+    test testStr1
+    test testStr2
+    test testStr3
+
+  test s = do
+    putStrLn s
+    putStrLn "=>"
+    print $ parsePHP s
+
